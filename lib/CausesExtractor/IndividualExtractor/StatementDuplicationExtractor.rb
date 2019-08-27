50c56ee18222ab75277ef9b1d900614e86dad2dc
João Gabriel Andrade <joaogabriel1598@gmail.com>
2019-08-27 15:56:44 -0300

    Extraction of information from gradle logs

--- lib/CausesExtractor/IndividualExtractor/StatementDuplicationExtractor.rb
+++ lib/CausesExtractor/IndividualExtractor/StatementDuplicationExtractor.rb
@@ -30,5 +30,33 @@ class StatementDuplicationExtractor
 			return "statementDuplication", [], 0
 		end
 	end
-	
+
+
+	def extractionFilesInfoForGradle(buildLog)
+		filesInformation = []
+		numberOccurrences = buildLog.scan(/[A-Za-z]*\.(java)(:)(\d+)(:)( error: )([a-z]*)( )([A-Za-z0-9]*)/).size
+		begin
+			information = buildLog.to_enum(:scan,/[A-Za-z]*\.(java)(:)(\d+)(:)( error: )([a-z]*)( )([A-Za-z0-9]*)/).map { Regexp.last_match }
+			count = 0
+			while(count < information.size)
+				classFile = information[count].to_s.match(/[A-Za-z]*\.(java)/)[0]
+				variableName = ""
+				if (information[count].to_s.match(/variable/) and information[count].to_s.match(/defined in method/))
+					variableName = information[count].to_s.match(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\,]*\]\s[a-zA-Z0-9\/\-\_]* [a-zA-Z0-9]*/)[0].split(" ").last
+				else
+					variableName = "method"
+				end
+				methodName = information[count].to_s.match(/(method )([A-Za-z0-9]*)/)[0].split(" ")[1]
+				count += 1
+				if (!methodName.include? ".")
+					filesInformation.push(["statementDuplication", classFile, variableName, methodName])
+				end
+			end
+			return "statementDuplication", filesInformation, information.size
+		rescue
+			return "statementDuplication", [], 0
+		end
+	end
+
 end
+
